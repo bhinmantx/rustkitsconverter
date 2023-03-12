82052cfd853c4e629c0edbf8e81ee14187535a06
Brendan H <brendan.hinman@gmail.com>
2023-03-12 18:07:50 -0500

    Update parsing (#2)
    * Moved to main.go
    Moved example files
    Removed comments
    Implemented CLI
    
    * Removing old parser (was for the .csv version!)
    
    * Indents, and adding item conversion
    
    * Removed comments. Added item definitions

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	var oldkits_filename, output_filename string
-	fmt.Println("About to parse vars. Guess it's slow when it's empty?")
+
 	flag.StringVar(&oldkits_filename, "old", "Kits.json", "name of the old kits file")
 	flag.StringVar(&output_filename, "new", "kits_data.json", "name of the new kits file")
 	flag.Parse()
--- model.go
+++ model.go
@@ -22,48 +22,6 @@ type Kit struct {
 	BeltItems          []KitItem `json:""`
 }
 
-/*
- {
-          "Shortname": "rifle.m39",
-          "Skin": 0,
-          "Amount": 1,
-          "Condition": 100.0,
-          "MaxCondition": 100.0,
-          "Ammo": 20,
-          "Ammotype": "ammo.rifle.hv",
-          "Position": 19,
-          "Frequency": -1,
-          "BlueprintShortname": null,
-          "Contents": [
-            {
-              "Shortname": "weapon.mod.8x.scope",
-              "Skin": 0,
-              "Amount": 1,
-              "Condition": 300.0,
-              "MaxCondition": 300.0,
-              "Ammo": 0,
-              "Ammotype": null,
-              "Position": 0,
-              "Frequency": -1,
-              "BlueprintShortname": null,
-              "Contents": null
-            },
-            {
-              "Shortname": "weapon.mod.lasersight",
-              "Skin": 0,
-              "Amount": 1,
-              "Condition": 300.0,
-              "MaxCondition": 300.0,
-              "Ammo": 0,
-              "Ammotype": null,
-              "Position": 1,
-              "Frequency": -1,
-              "BlueprintShortname": null,
-              "Contents": null
-            }
-          ]
-        }
-*/
 type KitItem struct {
 	Shortname          string    `json:"ShortName"`
 	Skin               int       `json:"Skin"`
@@ -77,37 +35,6 @@ type KitItem struct {
 	BlueprintShortname string    `json:"BlueprintShortname"`
 	Contents           []KitItem `json:"Contents"`
 }
-
-/*
-
-type OldKitItem struct {
-	KitName         string `json:"-",omitempty`
-	ItemID          int    `json:"itemid"`
-	Container       string `json:"container"`
-	Amount          int    `json:"amount"`
-	SkinID          int    `json:"skinid"`
-	Weapon          bool   `json:"weapon"`
-	BlueprintTarget int    `json:"blueprintTarget"`
-	Mods            []int  `json:"mods"`
-}
-
-
-
-type OldKit struct {
-	Name        string       `json:"name"`
-	Description *string      `json:"description"`
-	Max         int64        `json:"max"`
-	Cooldown    float64      `json:"cooldown"`
-	Authlevel   int64        `json:"authlevel"`
-	Hide        bool         `json:"hide"`
-	NpcOnly     bool         `json:"npconly"`
-	Permissions *string      `json:"permission"`
-	Image       *string      `json:"image"`
-	Building    *string      `json:"building"`
-	Items       []OldKitItem `json:"items"`
-}
-
-*/
 
 type OldKit struct {
 	Name        string       `json:"name"`
@@ -132,9 +59,3 @@ type OldKitItem struct {
 	BlueprintTarget int    `json:"blueprintTarget"`
 	Mods            []int  `json:"mods"`
 }
-
-/*
-	"mods": [
-		-132516482,
-		174866732
-	] */
